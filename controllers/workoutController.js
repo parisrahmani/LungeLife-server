5362b497871130efe9b115313b397559a75f3d62
parisrahmani <parisa.rahmani68@gmail.com>
2025-02-17 23:59:37 -0500

    Dev: add workout template controller

---
+++ controllers/workoutController.js
@@ -0,0 +1,34 @@
+import initKnex from "knex";
+import configuration from "../knexfile.js";
+
+const knex = initKnex(configuration);
+
+export async function getWorkoutTemplates(req, res) {
+  try {
+    const templates = await knex("workout_templates").select("*");
+    res.json(templates);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error fetching workout templates", error });
+  }
+}
+
+export async function addWorkoutTemplate(req, res) {
+  try {
+    const { user_id, template_name, date, exercises, notes } = req.body;
+
+    if (!user_id || !template_name || !date || !exercises) {
+      return res.status(400).json({ message: "Missing required fields" });
+    }
+
+    const [newTemplate] = await knex("workout_templates").insert(
+      { user_id, template_name, date, exercises, notes },
+      ["*"]
+    );
+
+    res.status(201).json(newTemplate);
+  } catch (error) {
+    res.status(500).json({ message: "Error adding workout template", error });
+  }
+}
