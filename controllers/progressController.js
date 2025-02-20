b8ffde1ff810dd0da43b283f2fd6820a01eb34d6
parisrahmani <parisa.rahmani68@gmail.com>
2025-02-20 18:18:39 -0500

    Dev: added post progress functionality

--- controllers/progressController.js
+++ controllers/progressController.js
@@ -3,32 +3,31 @@ import configuration from "../knexfile.js";
 
 const knex = initKnex(configuration);
 
-// export async function addProgress(req, res) {
-//   try {
-//     const { user_id, exercise_id, date, exerciseRecords } = req.body;
+export async function addProgress(req, res) {
+  try {
+    const { user_id, exercise_id, date, exerciseRecords } = req.body;
 
-//     if (!user_id || !exercise_id || !exerciseRecords || !date) {
-//       return res.status(400).json({ error: "Missing required fields" });
-//     }
+    if (!user_id || !exercise_id || !exerciseRecords || !date) {
+      return res.status(400).json({ error: "Missing required fields" });
+    }
 
-//     const formattedRecords = exerciseRecords.map((exercise) => ({
-//       id: knex.raw("UUID()"),
-//       user_id,
-//       exercise_id,
-//       date,
-//       weight: exercise.weight,
-//       reps: exercise.reps,
-//       sets: exercise.sets,
-//       duration: exercise.duration,
-//       prs: exercise.prs,
-//     }));
-//     await knex("progress").insert(formattedRecords);
+    const formattedRecords = exerciseRecords.map((exercise) => ({
+      id: knex.raw("UUID()"),
+      user_id,
+      exercise_id,
+      date,
+      weight: exercise.weight,
+      reps: exercise.reps,
+      duration: exercise.duration,
+      prs: exercise.prs,
+    }));
+    await knex("progress").insert(formattedRecords);
 
-//     res.status(201).json({ message: "Exercise records added successfully" });
-//   } catch (error) {
-//     res.status(500).json({ message: "Error adding Exercise records", error });
-//   }
-// }
+    res.status(201).json({ message: "Exercise records added successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Error adding Exercise records", error });
+  }
+}
 
 export const getProgressByUser = async (req, res) => {
   try {
@@ -40,46 +39,46 @@ export const getProgressByUser = async (req, res) => {
   }
 };
 
-export async function addProgress(req, res) {
-  try {
-    //const { user_id, exercise_id, date, eachExerciseRecords } = req.body;
-    const { exercises } = req.body;
+// export async function addProgress(req, res) {
+//   try {
+//     //const { user_id, exercise_id, date, eachExerciseRecords } = req.body;
+//     const { exercises } = req.body;
 
-    if (!Array.isArray(exercises) || exercises.length === 0) {
-      return res.status(400).json({ error: "Invalid request data" });
-    }
+//     if (!Array.isArray(exercises) || exercises.length === 0) {
+//       return res.status(400).json({ error: "Invalid request data" });
+//     }
 
-    let allRecords = [];
+//     let allRecords = [];
 
-    exercises.forEach(({ user_id, exercise_id, date, eachExerciseRecords }) => {
-      if (
-        !user_id ||
-        !exercise_id ||
-        !eachExerciseRecords ||
-        !date ||
-        !Array.isArray(eachExerciseRecords) ||
-        eachExerciseRecords.length === 0
-      ) {
-        return res.status(400).json({ error: "Missing required fields" });
-      }
+//     exercises.forEach(({ user_id, exercise_id, date, eachExerciseRecords }) => {
+//       if (
+//         !user_id ||
+//         !exercise_id ||
+//         !eachExerciseRecords ||
+//         !date ||
+//         !Array.isArray(eachExerciseRecords) ||
+//         eachExerciseRecords.length === 0
+//       ) {
+//         return res.status(400).json({ error: "Missing required fields" });
+//       }
 
-      const formattedRecords = eachExerciseRecords.map((exercise) => ({
-        id: knex.raw("UUID()"),
-        user_id,
-        exercise_id,
-        date,
-        weight: exercise.weight,
-        reps: exercise.reps,
-        duration: exercise.duration,
-        prs: exercise.prs,
-      }));
+//       const formattedRecords = eachExerciseRecords.map((exercise) => ({
+//         id: knex.raw("UUID()"),
+//         user_id,
+//         exercise_id,
+//         date,
+//         weight: exercise.weight,
+//         reps: exercise.reps,
+//         duration: exercise.duration,
+//         prs: exercise.prs,
+//       }));
 
-      allRecords = [...allRecords, ...formattedRecords];
-    });
-    await knex("progress").insert(allRecords);
+//       allRecords = [...allRecords, ...formattedRecords];
+//     });
+//     await knex("progress").insert(allRecords);
 
-    res.status(201).json({ message: "Exercise records added successfully" });
-  } catch (error) {
-    res.status(500).json({ message: "Error adding Exercise records", error });
-  }
-}
+//     res.status(201).json({ message: "Exercise records added successfully" });
+//   } catch (error) {
+//     res.status(500).json({ message: "Error adding Exercise records", error });
+//   }
+// }
