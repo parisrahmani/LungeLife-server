f4ada8dd9650be85f879b6872e1a0b77fb833674
parisrahmani <parisa.rahmani68@gmail.com>
2025-02-20 14:44:57 -0500

    Ref: add workout routecto get template  by id

--- routes/workoutRoutes.js
+++ routes/workoutRoutes.js
@@ -2,11 +2,13 @@ import express from "express";
 import {
   getWorkoutTemplates,
   addWorkoutTemplate,
+  getWorkoutExercises,
 } from "../controllers/workoutController.js";
 
 const router = express.Router();
 
 router.get("/", getWorkoutTemplates);
+router.get("/:id", getWorkoutExercises);
 router.post("/add", addWorkoutTemplate);
 
 export default router;
