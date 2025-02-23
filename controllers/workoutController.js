525fefe6a91e214699546d50a24e1105b912b708
parisrahmani <parisa.rahmani68@gmail.com>
2025-02-23 10:50:41 -0500

    Dev: defined functions to be able to add and update templates

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -55,3 +55,59 @@ export async function getWorkoutExercises(req, res) {
       .json({ message: "Error fetching workout exercises", error });
   }
 }
+
+export async function deleteTemplate(req, res) {
+  const { id } = req.params.id;
+  try {
+    const rowDeleted = await knex("workout_templates").where({ id }).delete();
+
+    if (rowDeleted === 0) {
+      return res
+        .status(404)
+        .json({ message: `Template with ID ${id} not found` });
+    }
+
+    res.sendStatus(204);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: `Unable to delete the template; ${error}` });
+  }
+}
+
+export async function updateTemplate(req, res) {
+  try {
+    const { id } = req.params;
+
+    const { user_id, template_name, exercises, notes, date } = req.body;
+
+    if (!user_id || !template_name || !exercises || !notes || !date) {
+      return res.status(400).json({ error: "All fields are required" });
+    }
+
+    const existingTemplate = await knex("workout_templates")
+      .where({ id })
+      .first();
+    if (!existingTemplate) {
+      return res
+        .status(404)
+        .json(`Could not find a template with id:${req.params.id} to update`);
+    }
+
+    await knex("workout_templates").where({ id }).update({
+      user_id,
+      template_name,
+      exercises,
+      notes,
+      date,
+    });
+
+    const updatedTemplate = await knex("workout_templates")
+      .where({ id: req.params.id })
+      .first();
+    res.status(200).json(updatedTemplate);
+  } catch (err) {
+    console.log(`Error updating template with id: ${req.params.id}.${err}`);
+    res.status(500).send(`Error updating template with id: ${req.params.id}`);
+  }
+}
